b49b3504eabbbff20eb7d94d1269b35fc920eda4
AntVelc <t.velciu@gmail.com>
2022-05-23 00:32:34 +0100

    2 positive numbers with operand

--- RubyCalculator.rb
+++ RubyCalculator.rb
@@ -1,18 +1,99 @@
 def calculator(calculus)
+  calculus += "s"
+  cn = calculus.length
+  n = 0
+  numberCounter = 0
+  nr1 = ""
+  nr2 = ""
+  final = 0
+  operand = ""
+
   
-  puts calculus
+  while n < cn
+    if calculus[n].count("0-9") > 0 
+
+      if operand == "" 
+	      nr1 = nr1 + calculus[n]
+	      n += 1
+	      numberCounter += 1
+      end
+      
+      if operand == "+" || operand == "-" || operand == "*"|| operand == "/"
+	      nr2 = nr2 + calculus[n]
+	      n += 1
+	      numberCounter += 1
+      end
+      
+    elsif (operand == "+" && calculus[n] = "s") || calculus[n] == "+"
+      if operand == ""
+        operand = operand + "+"
+        n += 1
+      elsif operand == "+" 
+	      final = nr1.to_i + nr2.to_i
+	      numberCounter = 0
+        nr = ""
+        nr2 = ""
+        operand = ""
+        n += 1
+      end
+	  elsif (operand == "-" && calculus[n] = "s") || calculus[n] == "-"
+      if operand == ""
+        operand += "-"
+        n += 1
+      elsif operand == "-"
+	      final = nr1.to_i - nr2.to_i
+	      n += 1
+	      numberCounter = 0
+        nr1 = ""
+        nr2 = ""
+        operand = ""
+      end
+      
+	  elsif (operand == "*" && calculus[n] = "s") || calculus[n] == "*"
+      
+      if operand == ""
+        operand += "*"
+        n += 1
+      elsif operand == "*"
+	      final = nr1.to_i * nr2.to_i
+	      n += 1
+	      numberCounter = 0
+        nr = ""
+        nr2 = ""
+        operand = ""
+      end
+      
+    elsif (operand == "/" && calculus[n] = "s") || calculus[n] == "/"
+      if operand == ""
+        operand += "/"
+        n += 1
+      elsif operand == "/"	    
+        final = nr1.to_i / nr2.to_i
+	      n += 1
+	      numberCounter = 0
+        nr = ""
+        nr2 = ""
+        operand = ""
+      end
+    end
+	end
+
+  puts final
 
   ready = gets.chomp
 end
 
-puts "please write numbers to be calculated along operations"
+puts "please write numbers to be calculated along with operands"
 
-calculation_string = gets.chomp
+m = 0
 
-calcReady = calculation_string.delete(' ')
-
-if calcReady.count("+-/*") > 0 && calcReady[0].count("0-9") > 0 && calcReady[-1].count("0-9") > 0
-  calculator(calcReady)
+while m < 1
+  calculation_string = gets.chomp
+  if calculation_string.count("+-/*") > 0 && calculation_string[0].count("0-9") > 0 && calculation_string[-1].count("0-9") > 0
+    m +=1
+    calcReady = calculation_string.delete(' ')
+    calculator(calcReady)
 else
-  puts "please put in at least a few numbers and operations"
+    puts "please put in only numbers and operands, as well as to start and finish with numbers"
+  end
 end
